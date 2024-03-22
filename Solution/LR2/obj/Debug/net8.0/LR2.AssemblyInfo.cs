ccb46d4af5ec077d2947e8766c6e87eb105fb3ed
heyuaresocute <nastya.gummy@gmail.com>
2024-03-22 17:00:00 +0300

    Add gitIgnore

--- Solution/LR2/obj/Debug/net8.0/LR2.AssemblyInfo.cs
+++ Solution/LR2/obj/Debug/net8.0/LR2.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("LR2")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+717beb876dc449deead8037e7edc35555dc429ff")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("LR2")]
 [assembly: System.Reflection.AssemblyTitleAttribute("LR2")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
