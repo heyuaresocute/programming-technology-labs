702d53325b1dfafa6248b3fefd3d6e7b45953f26
heyuaresocute <nastya.gummy@gmail.com>
2024-05-06 15:48:56 +0300

    1

--- Solution/LR4Tests/obj/Release/net8.0/LR4Tests.AssemblyInfo.cs
+++ Solution/LR4Tests/obj/Release/net8.0/LR4Tests.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("LR4Tests")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+679fe16e943eec31508966d633d9aa9527be4c18")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("LR4Tests")]
 [assembly: System.Reflection.AssemblyTitleAttribute("LR4Tests")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
